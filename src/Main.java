c781eaa8caabd7df9c95d4ba771b16997b9938de
Andrey Igumnov <igumnov92@gmail.com>
2022-10-13 12:34:25 +0300

    Реализация сериализации

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,15 @@ public class Main {
         Basket basket = new Basket();
         System.out.println(basket);
         if (basket.getFile().exists()) {
-            basket = Basket.loadFromTxtFile(basket.getFile());
+            basket = Basket.loadFromBinFile(basket.getFile());
             basket.printCart();
         } else {
             basket.addToCart(1, 20);
-            basket.saveTxt(basket.getFile());
+            basket.saveBin(basket.getFile());
             basket.addToCart(2, 3);
-            basket.saveTxt(basket.getFile());
+            basket.saveBin(basket.getFile());
             basket.addToCart(3, 2);
-            basket.saveTxt(basket.getFile());
+            basket.saveBin(basket.getFile());
             basket.printCart();
         }
     }
